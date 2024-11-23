034e54cfba80ebe919e5d03ce20881cc363c012b
Emanuel Peña <emanuel.cuevas03@gmail.com>
2024-11-23 20:02:11 +0100

    feat: gw settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from 'src/config';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
+  const LOGGER = new Logger('Main - API Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
+
+  await app.listen(envs.port);
+
+  LOGGER.log(`API Gateway running on port ${envs.port}`);
 }
 bootstrap();
